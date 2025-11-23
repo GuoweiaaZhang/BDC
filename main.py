4ced495cbd08dfa9dcdb759010771058c2f2ac3b
Zhang Guowei <111980248+GuoweiaaZhang@users.noreply.github.com>
2025-11-23 20:10:23 +0800

    Update main.py

--- main.py
+++ main.py
@@ -134,19 +134,24 @@ if torch.cuda.is_available():
                 features_te, _ = CNN_te(s1_x, perturb=True)
                 l0, l1, l2 = CNN_te.l0, CNN_te.l1, CNN_te.l2
                 l0_st, l1_st, l2_st = CNN_st.l0, CNN_st.l1, CNN_st.l2
+                def l2n(f, eps=1e-6):
+                    return f / (f.norm(p=2, dim=1, keepdim=True) + eps)
+                B = l0_st.size(0)
+                l0_st_f = l2n(l0_st.view(B, -1))
+                l0_f = l2n(l0.view(B, -1))
                 #  loss coral
-                coral_0 = CORAL(l0_st.view(40, -1), l0.view(40, -1))
+                coral_0 = CORAL(l0_st_f, l0_f)
                 coral_1 = CORAL(l1_st.view(40, -1), l1.view(40, -1))
                 coral_2 = CORAL(l2_st.view(40, -1), l2.view(40, -1))
-                loss_2 = - 0.1 * (coral_0)
+                loss_2 = - 100 * (coral_0)
 
             amp_grad_scaler.scale(loss_2).backward()
             amp_grad_scaler.unscale_(optimizer_LD)
             amp_grad_scaler.step(optimizer_LD)
             amp_grad_scaler.update()
 
-        if loss_1 is not None:
-            loss_1_list.append(loss_1.detach().cpu().numpy())
+        if Loss_1 is not None:
+            loss_1_list.append(Loss_1.detach().cpu().numpy())
             mean_loss_1 = np.mean(loss_1_list)
         else:
             mean_loss_1 = 0
@@ -187,3 +192,4 @@ if torch.cuda.is_available():
             print(
                 '==> Epoch: {}/{}, Loss_1: {:.5f}, Loss_2: {:.5f}, Test_Acc: {:.5f}, Recall: {:.4f}, F1: {:.4f}, Running time is {:.5f} s'.format(
                     i + 1, epoch, mean_loss_1, mean_loss_2, test_acc, recall_ave_class, f1, time_len))
+
